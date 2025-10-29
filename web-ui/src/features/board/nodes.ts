61ce9f8bbff894382efba20b882caac94f34780b
Виталий Степанов <vogistv@gmail.com>
2025-10-29 07:57:43 +0300

    add func select stickers ui

---
+++ web-ui/src/features/board/nodes.ts
@@ -0,0 +1,60 @@
+import { useState } from "react";
+
+type NodeBase = {
+  id: string;
+  type: string;
+};
+
+type StickerNode = NodeBase & {
+  type: "sticker";
+  text: string;
+  x: number;
+  y: number;
+};
+
+type Node = StickerNode;
+
+export function useNodes() {
+  const [nodes, setNodes] = useState<Node[]>([
+    {
+      id: "1",
+      type: "sticker",
+      text: "Hello",
+      x: 100,
+      y: 100,
+    },
+    {
+      id: "2",
+      type: "sticker",
+      text: "Hello2",
+      x: 200,
+      y: 200,
+    },
+  ]);
+
+  const addSticker = ({
+    text,
+    x,
+    y,
+  }: {
+    text: string;
+    x: number;
+    y: number;
+  }) => {
+    setNodes((prevNodes) => [
+      ...prevNodes,
+      {
+        id: crypto.randomUUID(),
+        type: "sticker",
+        text,
+        x: x,
+        y: y,
+      },
+    ]);
+  };
+
+  return {
+    nodes,
+    addSticker,
+  };
+}
