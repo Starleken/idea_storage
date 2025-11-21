7c852d0858a40619344df8fc4187d1a7226412c3
Starleken <starleken@mail.ru>
2025-11-21 19:19:11 +0700

    add validation

--- project/src/main/java/com/protobin/project/dto/tag/TagCreateAllDto.java
+++ project/src/main/java/com/protobin/project/dto/tag/TagCreateAllDto.java
@@ -1,5 +1,7 @@
 package com.protobin.project.dto.tag;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Builder;
 import lombok.Data;
 
@@ -9,8 +11,16 @@ import java.util.UUID;
 
 @Data
 @Builder
+@Schema(title = "Создание тэгов",
+        description = "Поля необходимые для создания")
 public class TagCreateAllDto {
 
+    @Schema(description = "Список тэгов")
+    @NotEmpty
     private List<String> names;
+
+
+    @Schema(description = "Идентификатор проекта", example = "3d5f1a26-c554-4bfe-811a-0e9ef2ed52fc")
+    @org.hibernate.validator.constraints.UUID
     private UUID projectId;
 }
--- project/src/main/java/com/protobin/project/dto/tag/TagResponseDto.java
+++ project/src/main/java/com/protobin/project/dto/tag/TagResponseDto.java
@@ -1,5 +1,6 @@
 package com.protobin.project.dto.tag;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Data;
 
@@ -7,8 +8,13 @@ import java.util.UUID;
 
 @Data
 @Builder
+@Schema(title = "Тэг",
+        description = "Поля получаемые с тэга")
 public class TagResponseDto {
 
+    @Schema(description = "Идентификатор тэга", example = "3d5f1a26-c554-4bfe-811a-0e9ef2ed52fc")
     private UUID id;
+
+    @Schema(description = "Значение тэга", example = "tag")
     private String name;
 }
